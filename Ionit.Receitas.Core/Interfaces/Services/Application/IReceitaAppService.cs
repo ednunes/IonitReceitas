fe1362bcac9a70eb1a03d19905ada30c79190e72
Eduardo Dantas <eduardo.dnunes@hotmail.com>
2017-09-11 20:42:07 -0300

    Implementados métodos de Inserir e Listar no serviço ReceitaAppService.

---
+++ Ionit.Receitas.Core/Interfaces/Services/Application/IReceitaAppService.cs
@@ -0,0 +1,13 @@
+﻿using Ionit.Receitas.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ionit.Receitas.Core.Interfaces.Services.Application
+{
+    public interface IReceitaAppService
+    {
+        int Inserir(ReceitaDto dto);
+        List<ReceitaConsultaCompletaDto> Listar();
+    }
+}
--- Ionit.Receitas.Core/Services/Application/ReceitaAppService.cs
+++ Ionit.Receitas.Core/Services/Application/ReceitaAppService.cs
@@ -4,11 +4,20 @@ using Ionit.Receitas.Core.Dto;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Ionit.Receitas.Core.Services.Domain;
+using Ionit.Receitas.Core.Interfaces.Services.Domain;
 
 namespace Ionit.Receitas.Core.Services.Application
 {
-    public class ReceitaAppService : IApplicationService
+    public class ReceitaAppService : IReceitaAppService
     {
+        protected readonly IDomainService<Receita> _service;
+
+        public ReceitaAppService(IDomainService<Receita> service)
+        {
+            _service = service;
+        }
+
         public int Alterar(ReceitaDto dto)
         {
             throw new NotImplementedException();
@@ -21,12 +30,59 @@ namespace Ionit.Receitas.Core.Services.Application
 
         public int Inserir(ReceitaDto dto)
         {
-            throw new NotImplementedException();
+            Receita entity = new Receita()
+            {
+                Titulo = dto.Titulo,
+                ModoPreparo = dto.ModoPreparo,
+                TempoPreparo = dto.TempoPreparo
+            };
+
+            foreach (ReceitaDto.Ingrediente ing in dto.Ingredientes)
+            {
+                Receita.Ingrediente ingrediente = new Receita.Ingrediente()
+                {
+                    Nome = ing.Nome,
+                    UnidadeMedida = ing.UnidadeMedida,
+                    Quantidade = ing.Quantidade
+                };
+
+                entity.Ingredientes.Add(ingrediente);
+            }
+
+            return _service.Inserir(entity);
         }
 
         public List<ReceitaConsultaCompletaDto> Listar()
         {
-            throw new NotImplementedException();
+            List<ReceitaConsultaCompletaDto> receitasDto = new List<ReceitaConsultaCompletaDto>();
+            
+            List<Receita> lst = _service.Listar();
+
+            foreach (Receita receita in lst)
+            {
+                ReceitaConsultaCompletaDto r = new ReceitaConsultaCompletaDto()
+                {
+                    Id = receita.Id,
+                    Titulo = receita.Titulo,
+                    ModoPreparo = receita.ModoPreparo,
+                    TempoPreparo = receita.TempoPreparo
+                };
+
+                foreach (Receita.Ingrediente ingrediente in receita.Ingredientes)
+                {
+                    r.Ingredientes.Add(new ReceitaConsultaCompletaDto.Ingrediente()
+                    {
+                        IdReceita = ingrediente.IdReceita,
+                        Nome = ingrediente.Nome,
+                        UnidadeMedida = ingrediente.UnidadeMedida,
+                        Quantidade = ingrediente.Quantidade
+                    });
+                }
+
+                receitasDto.Add(r);
+            }
+
+            return receitasDto;
         }
     }
 }
