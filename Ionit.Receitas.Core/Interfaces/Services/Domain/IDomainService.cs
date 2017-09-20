8ed11dec1c1ca10c87e59120e9eda1848ab3acee
Diego Balduino <homertrix_x@hotmail.com>
2017-09-20 19:57:10 -0300

    Precisando de melhor entendimento da arquitetura adotada.

--- Ionit.Receitas.Core/Interfaces/Services/Domain/IDomainService.cs
+++ Ionit.Receitas.Core/Interfaces/Services/Domain/IDomainService.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Ionit.Receitas.Core.Interfaces.Services.Domain
+﻿namespace Ionit.Receitas.Core.Interfaces.Services.Domain
 {
-    public interface IDomainService<ENTITY> 
-        where ENTITY : class
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Definição da interface de domínio do serviço.
+    /// </summary>
+    /// <typeparam name="Entity"></typeparam>
+    public interface IDomainService<Entity> where Entity : class
     {
-        ENTITY Consultar();
-        List<ENTITY> Listar();
-        int Inserir(ENTITY entity);
-        int Alterar(ENTITY entity);
+        #region Métodos
+
+        /// <summary>
+        /// Consultar Entidade pelo id.
+        /// </summary>
+        /// <param name="id">Id da entidade para consultar.</param>
+        /// <returns>Retorna uma entidade com o id passado ou null.</returns>
+        Entity Consultar(int id);
+
+        /// <summary>
+        /// Lista de Entidades.
+        /// </summary>
+        /// <returns>Retorna uma lista de entidades.</returns>
+        IEnumerable<Entity> Listar();
+
+        /// <summary>
+        /// Realizar a inserção da entidade no banco de dados.
+        /// </summary>
+        /// <param name="entity">Entidade a ser inserida.</param>
+        /// <returns>Retorna quantidade de linhas fetadas pela inserção da entidade.</returns>
+        int Inserir(Entity entity);
+
+        /// <summary>
+        /// Realizar a alteração da entidade no banco de dados.
+        /// </summary>
+        /// <param name="entity">Entidade a ser alterada.</param>
+        /// <returns>Retorna quantidade de linhas fetadas pela inserção da entidade.</returns>
+        int Alterar(Entity entity);
+
+        #endregion
     }
 }
