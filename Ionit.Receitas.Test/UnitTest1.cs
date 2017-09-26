12a5ea7d16927199945b176c178effcd8f8d0821
Fernando Albuquerque <fer.albuquerque90@gmail.com>
2017-09-25 22:42:03 -0300

    Teste de receitas

--- Ionit.Receitas.Test/UnitTest1.cs
+++ Ionit.Receitas.Test/UnitTest1.cs
@@ -1,3 +1,8 @@
+using Ionit.Receitas.Core.Entities;
+using Ionit.Receitas.Core.Interfaces.Services.Application;
+using Ionit.Receitas.Core.Interfaces.Services.Domain;
+using Ionit.Receitas.Core.Services.Application;
+using Moq;
 using System;
 using Xunit;
 
@@ -6,9 +11,22 @@ namespace Ionit.Receitas.Test
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void criando_uma_nova_receita()
         {
+            var domainService = new Mock<IDomainService<ReceitaDto>>();
 
+            //Instancia da dto Receita
+            var receita = new Mock<ReceitaDto>();
+
+            var receitaService = new ReceitaAppService(domainService.Object);
+
+            domainService.Setup(x => x.Inserir(receita.Object)).Returns(1);
+
+            
+
+            receita.SetupGet(x => x.Id).Returns(1);
+
+            Assert.Equal(0, receitaService.Inserir(receita.Object));
         }
     }
 }
