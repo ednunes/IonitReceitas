0bbd6cc08e996b73a05d562b9785fcdad5a51b89
Eduardo Dantas Nunes <eduardo.dnunes@hotmail.com>
2017-09-28 17:11:24 -0300

    Criação da interface IQueryRepository

--- Ionit.Receitas.Core/Interfaces/Repositories/IQueryRepositoryReceita.cs
+++ Ionit.Receitas.Core/Interfaces/Repositories/IQueryRepositoryReceita.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace Ionit.Receitas.Core.Interfaces.Repositories
 {
-    public interface IQueryRepositoryReceita
+    public interface IQueryRepositoryReceita<Entity>
     {
+        #region Métodos
+
+        /// <summary>
+        /// Consultar Entidade pelo id.
+        /// </summary>
+        /// <param name="id">Id da entidade para consultar.</param>
+        /// <returns>Retorna uma entidade com o id passado ou null.</returns>
+        Entity Consultar(int id);
+
+        /// <summary>
+        /// Lista de Entidades.
+        /// </summary>
+        /// <returns>Retorna uma lista de entidades.</returns>
+        IEnumerable<Entity> Listar();
+
+        #endregion
     }
 }
